de02b1bf5c9388695f89ebf4e0ec44c0608343c3
Ramon Cunha <ramon.network@hotmail.com>
2023-12-04 20:14:42 -0300

    Ajustado style Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <header className='fixed top-0 z-50 flex w-full items-center justify-between px-2 py-2 lg:px-10 lg:py-6 bg-black'>
+        <header className='fixed top-0 z-50 flex w-full items-center justify-between transition-all px-2 py-2 lg:px-10 lg:py-6'>
             <div className='flex items-center space-x-2 md:space-x-8'>
                 <img src='https://rb.gy/ulxxee'
                 alt='netflix'
@@ -14,6 +14,13 @@ export default function Header() {
                     <li>Movies</li>
                     <li>Latest</li>
                 </ul>
+            </div>
+
+            <div className='flex items-center space-x-4'>
+                <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+                <img src='https://rb.gy/g1pwyx'
+                alt=''
+                className='cursor-pointer rounded'/>
             </div>
         </header>
     );
