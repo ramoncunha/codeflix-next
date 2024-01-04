779655ba6fe2f4c1494e504cbc5091c16c4dc0b4
Ramon Cunha <ramon.network@hotmail.com>
2024-01-03 22:39:53 -0300

    Adicionado heroicons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
+import { PlayIcon } from '@heroicons/react/24/solid';
 
 export default function Home() {
   return (
@@ -28,10 +30,10 @@ export default function Home() {
 
         <div className='flex space-x-3'>
           <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-            Play
+            <PlayIcon className="h-6" /> Play
           </button>
           <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-            More Info
+            <InformationCircleIcon className='h-6'/> More Info
           </button>
         </div>
         
